fb56b5da73d5146e44c2c97e8d42f18645e04d6f
amsurio <sim.amsurio@gmail.com>
2024-08-29 16:40:45 +0200

    init

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="min-h-screen bg-black text-white flex flex-col items-center relative">
+    <div class="black-background relative z-10">
+      <Navbar />
+      <Hero />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/navbar.vue';
+import Hero from '@/components/hero.vue';
+
+export default {
+  components: {
+    Navbar,
+    Hero
+  }
+}
+</script>
+
+<style scoped>
+
+.black-background {
+  width: 100%;
+}
+</style>
+
+
